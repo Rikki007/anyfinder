b9c8e17082db9c40a9c14d86d96978e2760b307f
Kirill Oreshkevich <Oreshaodnoglazy@gmail.com>
2024-03-19 13:22:21 +0300

    feat: add html worked on css

---
+++ index.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" href="./img/favicon.svg" type="image/x-icon">
+  <link rel="stylesheet" href="style.css">
+  <title>anyfinder</title>
+</head>
+<body>
+  <header class="header">
+    <div class="container">
+      <img src="./img/logo-min.png" alt="icon" class="logo">
+      <nav class="navbar">
+        <ul class="navlist">
+          <li class="navlist__item">
+            <a href="" class="navlist__link">Main</a>
+          </li>
+          <li class="navlist__item">
+            <a href="" class="navlist__link">Purchase</a>
+          </li>
+          <li class="navlist__item">
+            <a href="" class="navlist__link">Sale</a>
+          </li>
+          <li class="navlist__item">
+            <a href="" class="navlist__link">Log in</a>
+          </li>
+          <li class="navlist__item">
+            <a href="" class="navlist__link">Register</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+  <main class="main">
+    <article>
+      <section class="ads">
+        <div class="card">
+          <img src="./img/camera-off-svgrepo-com.svg" alt="icon" class="card__img">
+          <p class="date">some date</p>
+          <h3 class="purchase-sale">purchase/sale</h3>
+          <p class="product">prod</p>
+          <p class="description">desc</p>
+          <h3 class="price">price</h3>
+        </div>
+        <div class="card">
+          <img src="./img/camera-off-svgrepo-com.svg" alt="icon" class="card__img">
+          <p class="date">some date</p>
+          <h3 class="purchase-sale">purchase/sale</h3>
+          <p class="product">prod</p>
+          <p class="description">desc</p>
+          <h3 class="price">price</h3>
+        </div>
+        <div class="card">
+          <img src="./img/camera-off-svgrepo-com.svg" alt="icon" class="card__img">
+          <p class="date">some date</p>
+          <h3 class="purchase-sale">purchase/sale</h3>
+          <p class="product">prod</p>
+          <p class="description">desc</p>
+          <h3 class="price">price</h3>
+        </div>
+        <div class="card">
+          <img src="./img/camera-off-svgrepo-com.svg" alt="icon" class="card__img">
+          <p class="date">some date</p>
+          <h3 class="purchase-sale">purchase/sale</h3>
+          <p class="product">prod</p>
+          <p class="description">desc</p>
+          <h3 class="price">price</h3>
+        </div>
+        <div class="card">
+          <img src="./img/camera-off-svgrepo-com.svg" alt="icon" class="card__img">
+          <p class="date">some date</p>
+          <h3 class="purchase-sale">purchase/sale</h3>
+          <p class="product">prod</p>
+          <p class="description">desc</p>
+          <h3 class="price">price</h3>
+        </div>
+        <div class="card">
+          <img src="./img/camera-off-svgrepo-com.svg" alt="icon" class="card__img">
+          <p class="date">some date</p>
+          <h3 class="purchase-sale">purchase/sale</h3>
+          <p class="product">prod</p>
+          <p class="description">desc</p>
+          <h3 class="price">price</h3>
+        </div>
+        <div class="card">
+          <img src="./img/camera-off-svgrepo-com.svg" alt="icon" class="card__img">
+          <p class="date">some date</p>
+          <h3 class="purchase-sale">purchase/sale</h3>
+          <p class="product">prod</p>
+          <p class="description">desc</p>
+          <h3 class="price">price</h3>
+        </div>
+        <div class="card">
+          <img src="./img/camera-off-svgrepo-com.svg" alt="icon" class="card__img">
+          <p class="date">some date</p>
+          <h3 class="purchase-sale">purchase/sale</h3>
+          <p class="product">prod</p>
+          <p class="description">desc</p>
+          <h3 class="price">price</h3>
+        </div>
+        <div class="card">
+          <img src="./img/camera-off-svgrepo-com.svg" alt="icon" class="card__img">
+          <p class="date">some date</p>
+          <h3 class="purchase-sale">purchase/sale</h3>
+          <p class="product">prod</p>
+          <p class="description">desc</p>
+          <h3 class="price">price</h3>
+        </div>
+        <div class="card">
+          <img src="./img/camera-off-svgrepo-com.svg" alt="icon" class="card__img">
+          <p class="date">some date</p>
+          <h3 class="purchase-sale">purchase/sale</h3>
+          <p class="product">prod</p>
+          <p class="description">desc</p>
+          <h3 class="price">price</h3>
+        </div>
+        <div class="card">
+          <img src="./img/camera-off-svgrepo-com.svg" alt="icon" class="card__img">
+          <p class="date">some date</p>
+          <h3 class="purchase-sale">purchase/sale</h3>
+          <p class="product">prod</p>
+          <p class="description">desc</p>
+          <h3 class="price">price</h3>
+        </div>
+        <div class="card">
+          <img src="./img/camera-off-svgrepo-com.svg" alt="icon" class="card__img">
+          <p class="date">some date</p>
+          <h3 class="purchase-sale">purchase/sale</h3>
+          <p class="product">prod</p>
+          <p class="description">desc</p>
+          <h3 class="price">price</h3>
+        </div>
+        <div class="card">
+          <img src="./img/camera-off-svgrepo-com.svg" alt="icon" class="card__img">
+          <p class="date">some date</p>
+          <h3 class="purchase-sale">purchase/sale</h3>
+          <p class="product">prod</p>
+          <p class="description">desc</p>
+          <h3 class="price">price</h3>
+        </div>
+        <div class="card">
+          <img src="./img/camera-off-svgrepo-com.svg" alt="icon" class="card__img">
+          <p class="date">some date</p>
+          <h3 class="purchase-sale">purchase/sale</h3>
+          <p class="product">prod</p>
+          <p class="description">desc</p>
+          <h3 class="price">price</h3>
+        </div>
+        <div class="card">
+          <img src="./img/camera-off-svgrepo-com.svg" alt="icon" class="card__img">
+          <p class="date">some date</p>
+          <h3 class="purchase-sale">purchase/sale</h3>
+          <p class="product">prod</p>
+          <p class="description">desc</p>
+          <h3 class="price">price</h3>
+        </div>
+        <div class="card">
+          <img src="./img/camera-off-svgrepo-com.svg" alt="icon" class="card__img">
+          <p class="date">some date</p>
+          <h3 class="purchase-sale">purchase/sale</h3>
+          <p class="product">prod</p>
+          <p class="description">desc</p>
+          <h3 class="price">price</h3>
+        </div>
+      </section>
+    </article>
+  </main>
+  <footer class="footer">
+    <img src="./img/logo-min.png" alt="icon" class="logo">
+    <p class="rights">©2024. All rights reserved.</p>
+  </footer>
+</body>
+</html>
